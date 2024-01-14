e515b5b76104b5c5b2fada6ce6b99570caa49c19
Paula Markowska <paula.dziubek94@gmail.com>
2024-01-14 18:02:24 +0100

    PricingPosition component

--- my-app/src/Components/Pricing/Pricing.jsx
+++ my-app/src/Components/Pricing/Pricing.jsx
@@ -1,6 +1,7 @@
 
 import  styles  from "./Pricing.module.css";
 import classnames from "classnames";
+import { PricingPosition } from "./PricingPosition/PricingPosition";
 
 export const Pricing = () => {
     return (
@@ -8,7 +9,10 @@ export const Pricing = () => {
 <div className={styles["pricing__container"]} id="pricing">
 <h1 className={styles["pricing__header"]}>Our pricing</h1>
 <div className={styles["pricing__content-container"]}>
-  <div className={styles["pricing__option--single"]}>
+    <PricingPosition title = "single photo price" price = "10" description = "Already taken, No modification, Single use"></PricingPosition>
+    <PricingPosition title = "pack photo price" price = "200" description = "Already taken, No modification, Single use"></PricingPosition>
+    <PricingPosition title = "photo custom price" price = "call us" description = "Taken or new, Modifications, Multi use"></PricingPosition>
+  {/* <div className={styles["pricing__option--single"]}>
     <div className={styles["pricing__content"]}>
       <h3 className={styles["pricing__content-header"]}>Single photo price</h3>
       <div className={styles["pricing__value-container"]}>
@@ -52,7 +56,7 @@ export const Pricing = () => {
         </ul>
       </div>
     </div>
-  </div>
+  </div> */}
 </div>
 </div>
 </div>  )
---
+++ my-app/src/Components/Pricing/PricingPosition/PricingPosition.jsx
@@ -0,0 +1,25 @@
+import  styles  from "./PricingPosition.module.css";
+import classnames from "classnames";
+
+export const PricingPosition = (props) => {
+    return (
+<div>
+<div className={styles["pricing__option--single"]}>
+    <div className={styles["pricing__content"]}>
+      <h3 className={styles["pricing__content-header"]}>Single photo price</h3>
+      <div className={styles["pricing__value-container"]}>
+        <p>$10</p>
+      </div>
+      <div className={styles["pricing__description-container"]}>
+        <ul className={classnames(styles["pricing__description"], styles["pricing__description-minus"])}>
+          <li>Already taken</li>
+          <li>No modification</li>
+          <li>Single use</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+    )
+}
