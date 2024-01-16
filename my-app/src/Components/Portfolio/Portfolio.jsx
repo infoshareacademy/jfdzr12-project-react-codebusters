0f1354b8a726e12d075d5f7f0088f042ee3d1925
Halyna Riabokonenko <sgallina89@gmail.com>
2024-01-16 21:54:29 +0100

    use photo URLs from portfolio.json at Portfolio.jsx

--- my-app/src/Components/Portfolio/Portfolio.jsx
+++ my-app/src/Components/Portfolio/Portfolio.jsx
@@ -1,21 +1,31 @@
-import  styles  from "./Portfolio.module.css";
+import styles from "./Portfolio.module.css";
+import portfolioData from "../../portfolio.json";
 
 export const Portfolio = () => {
-    return (
-<div>
-<div className={styles["portfolio__container"]} id="portfolio">
-          <h1 className={styles["portfolio__header"]}>Our portfolio</h1>
-          <div className={styles["portfolio"]}>
-            <h3 className={styles["portfolio__info"]}>
-              We are experienced in taking photos for all kinds of
-              circumstances.<br />
-              Below is a summary of our extensive portfolio, with us you will
-              always look good!
-            </h3>
-          </div>
-          <div className={styles["portfolio__photos"]}></div>
-
+  return (
+    <div>
+      <div className={styles["portfolio__container"]} id="portfolio">
+        <h1 className={styles["portfolio__header"]}>Our portfolio</h1>
+        <div className={styles["portfolio"]}>
+          <h3 className={styles["portfolio__info"]}>
+            We are experienced in taking photos for all kinds of circumstances.
+            <br />
+            Below is a summary of our extensive portfolio, with us you will
+            always look good!
+          </h3>
         </div>
-</div>
-    )
-}
+        <div className={styles["portfolio__photos"]}>
+          {portfolioData.photos.map((photo) => (
+            <div key={photo.id} className={styles["portfolio__items"]}>
+              <img
+                src={photo.url}
+                className={styles["portfolio__photo"]}
+                data-id={photo.id}
+              ></img>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
