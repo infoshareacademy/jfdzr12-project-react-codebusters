529e2f57344c766b2dab8b5536d556b47ee6baf4
Halyna Riabokonenko <sgallina89@gmail.com>
2024-02-04 22:30:12 +0100

    change PhotoImage component, add stiles to PhotoImage component

--- my-app/src/Components/Portfolio/PortfolioImage/PortfolioImage.jsx
+++ my-app/src/Components/Portfolio/PortfolioImage/PortfolioImage.jsx
@@ -1,15 +1,25 @@
-import { useOutletContext } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import portfolioData from "../../../../public/photos.json";
-
+import styles from "./PortfolioImage.module.css";
 
 export const PortfolioImage = () => {
-    const imageId = useOutletContext();
-    const photo = portfolioData.photos.find(
-        (photo) => photo.id === parseInt(imageId)
-    );
-    return (
-        <div>
-            <img src={photo.url} />
-        </div>
-    )
-}
+  const { imageId } = useParams();
+
+  const photo = portfolioData.photos.find(
+    (el) => el.id == imageId.replace(":", "")
+  );
+
+  console.log("PHOTO", photo);
+  return (
+    <div className={styles["portfolio-image__content"]}>
+      <img src={photo.url} className={styles["portfolio-image__image"]} />
+      <div className={styles["portfolio-image__info"]}>
+        <h2>{photo.author}</h2>
+        <p>
+          {photo.height} x {photo.width}
+        </p>
+        <h3>Price: {photo.price}$</h3>
+      </div>
+    </div>
+  );
+};
