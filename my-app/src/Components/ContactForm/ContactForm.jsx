7e89bcd4704c0661ad9338dbb37447006ed9eb36
Halyna Riabokonenko <sgallina89@gmail.com>
2024-02-09 11:21:26 +0100

    add dark theme topages background, add styles to Not found

--- my-app/src/Components/ContactForm/ContactForm.jsx
+++ my-app/src/Components/ContactForm/ContactForm.jsx
@@ -18,71 +18,78 @@ export const ContactForm = () => {
   };
 
   return (
-    <div className={classnames(styles["modal"])} id="contact">
-      <h1 className={classnames(styles["modal__header"])}>Contact us</h1>
-      <form
-        action=""
-        method="get"
-        className={classnames(styles["modal__form"])}
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="title" className={classnames(styles["form__label"])}>
-          Title
-        </label>
-        <input
-          placeholder=""
-          type="text"
-          id="title"
-          name="title"
-          className={classnames(
-            styles["form__input"],
-            styles["form__input--text"]
-          )}
-          required
-        />
-        <label htmlFor="email" className={classnames(styles["form__label"])}>
-          Email
-        </label>
-        <input
-          ref={inputEmailEl}
-          placeholder=""
-          type="email"
-          id="email"
-          name="email"
-          className={classnames(
-            styles["form__input"],
-            styles["form__input--email"]
-          )}
-          required
-        />
-        <p className={classnames(styles["form__error-message"])}>
-          {emailMessage}
-        </p>
-        <label htmlFor="message" className={classnames(styles["form__label"])}>
-          Message
-        </label>
-        <textarea
-          placeholder=""
-          id="message"
-          className={classnames(
-            styles["form__input"],
-            styles["form__input--textarea"]
-          )}
-          minLength="20"
-          name="message"
-          rows="4"
-          cols="50"
-          required
-        ></textarea>
-        <input
-          type="submit"
-          value="Send"
-          className={classnames(
-            styles["form__input"],
-            styles["form__input--submit"]
-          )}
-        />
-      </form>
+    <div
+      className={classnames(styles["contact-form__container"], styles[theme])}
+    >
+      <div className={classnames(styles["modal"])} id="contact">
+        <h1 className={classnames(styles["modal__header"])}>Contact us</h1>
+        <form
+          action=""
+          method="get"
+          className={classnames(styles["modal__form"])}
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="title" className={classnames(styles["form__label"])}>
+            Title
+          </label>
+          <input
+            placeholder=""
+            type="text"
+            id="title"
+            name="title"
+            className={classnames(
+              styles["form__input"],
+              styles["form__input--text"]
+            )}
+            required
+          />
+          <label htmlFor="email" className={classnames(styles["form__label"])}>
+            Email
+          </label>
+          <input
+            ref={inputEmailEl}
+            placeholder=""
+            type="email"
+            id="email"
+            name="email"
+            className={classnames(
+              styles["form__input"],
+              styles["form__input--email"]
+            )}
+            required
+          />
+          <p className={classnames(styles["form__error-message"])}>
+            {emailMessage}
+          </p>
+          <label
+            htmlFor="message"
+            className={classnames(styles["form__label"])}
+          >
+            Message
+          </label>
+          <textarea
+            placeholder=""
+            id="message"
+            className={classnames(
+              styles["form__input"],
+              styles["form__input--textarea"]
+            )}
+            minLength="20"
+            name="message"
+            rows="4"
+            cols="50"
+            required
+          ></textarea>
+          <input
+            type="submit"
+            value="Send"
+            className={classnames(
+              styles["form__input"],
+              styles["form__input--submit"]
+            )}
+          />
+        </form>
+      </div>
     </div>
   );
 };
--- my-app/src/Components/Pricing/Pricing.jsx
+++ my-app/src/Components/Pricing/Pricing.jsx
@@ -2,6 +2,7 @@ import styles from "./Pricing.module.css";
 import { PricingPosition } from "./PricingPosition/PricingPosition";
 import { ThemeContext } from "../../providers/theme";
 import { useContext } from "react";
+import classnames from "classnames";
 
 export const Pricing = () => {
   const { theme } = useContext(ThemeContext);
@@ -23,7 +24,7 @@ export const Pricing = () => {
   ];
   return (
     <>
-      <div className={styles["pricing__container"]} id="pricing">
+      <div className={classnames(styles["pricing__container"], styles[theme])}>
         <h1 className={styles["pricing__header"]}>Our pricing</h1>
         <div className={styles["pricing__content-container"]}>
           <PricingPosition
@@ -41,50 +42,6 @@ export const Pricing = () => {
             price="Call us"
             description={descriptionCustomPhotos}
           ></PricingPosition>
-          {/* <div className={styles["pricing__option--single"]}>
-    <div className={styles["pricing__content"]}>
-      <h3 className={styles["pricing__content-header"]}>Single photo price</h3>
-      <div className={styles["pricing__value-container"]}>
-        <p>$10</p>
-      </div>
-      <div className={styles["pricing__description-container"]}>
-        <ul className={classnames(styles["pricing__description"], styles["pricing__description-minus"])}>
-          <li>Already taken</li>
-          <li>No modification</li>
-          <li>Single use</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div className={styles["pricing__option--pack"]}>
-    <div className={styles["pricing__content"]}>
-      <h3 className={styles["pricing__content-header"]}>Pack photo price</h3>
-      <div className={styles["pricing__value-container"]}>
-        <p>$200</p>
-      </div>
-      <div className={styles["pricing__description-container"]}>
-          <li>Already taken</li>
-          <li>No modification</li>
-          <li>Single use</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div className={styles["pricing__option--custom"]}>
-    <div className={styles["pricing__content"]}>
-      <h3 className={styles["pricing__content-header"]}>Photo custom price</h3>
-      <div className={styles["pricing__value-container"]}>
-        <p>Call us</p>
-      </div>
-      <div className={styles["pricing__description-container"]}>
-        <ul className={classnames(styles["pricing__description"], styles["pricing__description-plus"])}>
-          <li>Taken or new</li>
-          <li>Modifications</li>
-          <li>Multi use</li>
-        </ul>
-      </div>
-    </div>
-  </div> */}
         </div>
       </div>
     </>
